bfb3c6a36666b1822b28e8ce78b9bb8d9c987eff
soueip <71385302+soueip@users.noreply.github.com>
2022-06-12 11:42:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container, Form, Nav} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <Nav bsPrefix="nav1"
+  activeKey="/home"
+  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+    <Form>
+  <Form.Group className="mb-3" controlId="formGroupEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formGroupPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+</Form>
+    </div>
+  );
+}
+
+export default App;
